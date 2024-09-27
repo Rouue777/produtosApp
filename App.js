ae4b0483a3ab6f31765a149d4ee34ea720ea70c2
Jeferson santos <jefersonrouue@gmail.com>
2024-09-27 19:04:21 -0300

    front end home terminado

--- App.js
+++ App.js
@@ -42,6 +42,9 @@ app.use((req,res,next)=>{
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
+    res.locals.user = req.user || null ;
+    res.locals.eadmin = req.user || null;
+    res.locals.username = req.user ? req.user.nome : null; 
     next()
 })
 
