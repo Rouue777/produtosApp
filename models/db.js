065570f74f7dd69168c5d838add151e18a63df40
Jeferson santos <jefersonrouue@gmail.com>
2024-10-03 20:30:50 -0300

    connectTimeout added sequelize

--- models/db.js
+++ models/db.js
@@ -2,13 +2,16 @@ import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
     host: process.env.DB_HOSTNAME,
-    dialect : 'mysql'
-})
+    dialect : 'mysql',
+    dialectOptions: {
+        connectTimeout: 60000
+    }
+});
 
 sequelize.authenticate().then(() =>{
     console.log('banco de dados conectado com sucesso')
 }).catch((err)=>{
-    console.log('erro ao conctar ao banco de dados' + err)
+    console.log('erro ao conectar ao banco de dados' + err)
 })
 
 
