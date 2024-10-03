29ae99b8c08f6c2d3dd3ec302d6e2cf17a92df70
Jeferson santos <jefersonrouue@gmail.com>
2024-10-03 20:49:53 -0300

    adicionado variavel de ambiente porta

--- models/db.js
+++ models/db.js
@@ -3,6 +3,7 @@ import { Sequelize } from "sequelize";
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
     host: process.env.DB_HOSTNAME,
     dialect : 'mysql',
+    port: process.env.DB_PORT,
     dialectOptions: {
         connectTimeout: 60000
     }
