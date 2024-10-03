8beed39d3583526f779831468a1d966f9383f835
Jeferson santos <jefersonrouue@gmail.com>
2024-10-02 22:10:22 -0300

    variaveis de ambiente banco de dados

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize('produtosApp', 'root', 'sql123',{
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
+    host: process.env.DB_HOSTNAME,
     dialect : 'mysql'
 })
 
